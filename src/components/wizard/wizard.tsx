e0ad24e60fbb00f0a259644e20fb05d02073dceb
prado <pradodje@gmail.com>
2023-12-13 00:40:11 -0800

    Update docs with example

--- src/components/wizard/wizard.tsx
+++ src/components/wizard/wizard.tsx
@@ -5,8 +5,17 @@ import { WizardProvider } from './wizard-provider';
  * A Higher Order Component that wraps the given component in a
  * {@link WizardProvider}.
  *
+ * @example
+ * ```
+ * function Component() {
+ *  const { ... } = useWizardContext();
+ *  ...
+ * }
+ * export const WizardComponent = wizard(Component);
+ * ```
+ *
  * @param WrappedComponent A React component that acts as a wizard.
- * @returns The {@link WrappedComponent} wrapped in a {@link WizardProvider}.
+ * @returns The `WrappedComponent` wrapped in a {@link WizardProvider}.
  */
 export function wizard<P extends object>(WrappedComponent: ComponentType<P>) {
   return (props: P) => {
